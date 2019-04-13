f67843ffae094e7ad430d13ee972e84ed29a756b
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-13 14:37:23 +0200

    <title>, minor load page changes

--- loader.js
+++ loader.js
@@ -1,16 +1,24 @@
 var showLetters = false;
 
-var q = location.search.match(/^\??(.*)/)[1].split('&');
-if (!q.length) {
-	document.getElementById("errors").textContent = "Please add ?1, ?2, ... to the URL to load a specific crossword.";
+var errElm = document.getElementById("errors");
+var qs = location.search.match(/^\??(.*)/)[1], q = qs.split('&');
+if (!qs.length) {
+	// "Please add ?1, ?2, ... to the URL to load a specific crossword."
+	errElm.textContent = "Lägg till ?1, ?2, ... till URLen för att ladda ett specifikt korsord.";
 } else {
+	var num = q[0];
+	document.title += ' #' + num;
 	var scr = document.createElement("script");
 	if (q.includes("show=1")) showLetters = true;
-	scr.src = 'data/' + q[0] + '.js';
+	scr.src = 'data/' + num + '.js';
 	scr.onload = function() {
 		var scr = document.createElement("script");
 		scr.src = 'crossword.js';
 		document.body.appendChild(scr);
+	};
+	scr.onerror = function() {
+		// "No such crossword."
+		errElm.textContent = "Korsordet finns inte.";
 	};
 	document.body.appendChild(scr);
 }
