482dab70c9f5ea5b47f3285da96925754b2eb48d
Simon Lindholm <simon.lindholm10@gmail.com>
2019-04-21 23:30:10 +0200

    Move more code into function

--- crossword.js
+++ crossword.js
@@ -30,79 +30,81 @@ var clues = {
 };
 
 var height = special.length, width = special[0].length;
-if (haveGrid) console.assert(grid.length == height);
-for (var i = 0; i < height; i++) {
-	if (haveGrid) console.assert(grid[i].length == width);
-	console.assert(special[i].length == width);
-}
-
-var errorCont = document.getElementById("errors");
-function addError(msg) {
-	var div = document.createElement("div");
-	div.textContent = msg;
-	errorCont.appendChild(div);
-}
-
-function clueDirs(i, j) {
-	if (special[i][j] == 'D') return VERT | REVERSE;
-	if (special[i][j] == '#') return 0;
-	if (special[i][j] == 'A' || special[i][j] == 'B') return 0;
-	var res = 0;
-	if (i+1 < height && special[i+1][j] != '#' && (i == 0 || special[i-1][j] == '#')) res |= VERT;
-	if (j+1 < width && special[i][j+1] != '#' && (j == 0 || special[i][j-1] == '#')) res |= HOR;
-	return res;
-}
-
-var clueCtrs = {
-	vert: 0,
-	hor: 0,
-};
-function addClue(cat, index, cells) {
-	var ct = clueCtrs[cat];
-	if (haveGrid) {
-		var word = "";
-		for (let cell of cells) {
-			word += grid[cell.y][cell.x];
-		}
-		word = word.toLowerCase();
-		if (ct == clues[cat].length || !clues[cat].some(c => c.secret === word)) {
-			addError("Missing clue for " + cat + " word " + word);
-			return;
-		}
-		else if (clues[cat][ct].secret !== word) {
-			addError("Wrongly ordered clue " + word + ", expected it to come before " + clues[cat][ct].secret);
-			throw "stop";
-		}
-	}
-
-	clues[cat][ct].index = index;
-	clues[cat][ct].cells = cells;
-	clueCtrs[cat]++;
-}
-
-function followClue(i, j, dir) {
-	var ret = [];
-	while (i >= 0 && j >= 0 && i < height && j < width && special[i][j] != '#') {
-		var sp = special[i][j];
-		if (sp == 'C' && (dir == HOR)) break;
-		ret.push({y: i, x: j});
-		if (sp == 'A') dir = VERT;
-		if (sp == 'B') dir = HOR;
-		if (sp == 'C' && dir == (VERT | REVERSE)) dir = HOR;
-		if (dir == HOR) j++;
-		else if (dir == (HOR | REVERSE)) j--;
-		else if (dir == VERT) i++;
-		else if (dir == (VERT | REVERSE)) i--;
-		else throw new Error("invalid direction " + dir);
-	}
-	return ret;
-}
 
 function descSq(i, j) {
 	return "Row " + (i+1) + " col " + (j+1);
 }
 
-function go() {
+function init() {
+	var errorCont = document.getElementById("errors");
+	function addError(msg) {
+		var div = document.createElement("div");
+		div.textContent = msg;
+		errorCont.appendChild(div);
+	}
+
+	function clueDirs(i, j) {
+		if (special[i][j] == 'D') return VERT | REVERSE;
+		if (special[i][j] == '#') return 0;
+		if (special[i][j] == 'A' || special[i][j] == 'B') return 0;
+		var res = 0;
+		if (i+1 < height && special[i+1][j] != '#' && (i == 0 || special[i-1][j] == '#')) res |= VERT;
+		if (j+1 < width && special[i][j+1] != '#' && (j == 0 || special[i][j-1] == '#')) res |= HOR;
+		return res;
+	}
+
+	var clueCtrs = {
+		vert: 0,
+		hor: 0,
+	};
+
+	function addClue(cat, index, cells) {
+		var ct = clueCtrs[cat];
+		if (haveGrid) {
+			var word = "";
+			for (let cell of cells) {
+				word += grid[cell.y][cell.x];
+			}
+			word = word.toLowerCase();
+			if (ct == clues[cat].length || !clues[cat].some(c => c.secret === word)) {
+				addError("Missing clue for " + cat + " word " + word);
+				return;
+			}
+			else if (clues[cat][ct].secret !== word) {
+				addError("Wrongly ordered clue " + word + ", expected it to come before " + clues[cat][ct].secret);
+				throw "stop";
+			}
+		}
+
+		clues[cat][ct].index = index;
+		clues[cat][ct].cells = cells;
+		clueCtrs[cat]++;
+	}
+
+	function followClue(i, j, dir) {
+		var ret = [];
+		while (i >= 0 && j >= 0 && i < height && j < width && special[i][j] != '#') {
+			var sp = special[i][j];
+			if (sp == 'C' && (dir == HOR)) break;
+			ret.push({y: i, x: j});
+			if (sp == 'A') dir = VERT;
+			if (sp == 'B') dir = HOR;
+			if (sp == 'C' && dir == (VERT | REVERSE)) dir = HOR;
+			if (dir == HOR) j++;
+			else if (dir == (HOR | REVERSE)) j--;
+			else if (dir == VERT) i++;
+			else if (dir == (VERT | REVERSE)) i--;
+			else throw new Error("invalid direction " + dir);
+		}
+		return ret;
+	}
+
+	if (haveGrid) console.assert(grid.length == height);
+	for (var i = 0; i < height; i++) {
+		if (haveGrid) console.assert(grid[i].length == width);
+		console.assert(special[i].length == width);
+	}
+
 	var clueNum = 0;
 	var tbody = document.querySelector("tbody");
 	var clueCont = document.getElementById("clues");
@@ -177,7 +179,7 @@ function go() {
 }
 
 try {
-	go();
+	init();
 } catch(e) {
 	if (e !== "stop") throw e;
 }
