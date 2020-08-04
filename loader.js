0f3253c2f9c793f0fcf9b560b63eaf949f58b52c
Simon Lindholm <simon.lindholm10@gmail.com>
2020-08-04 23:06:16 +0200

    Fall back to non-key version if it exists

--- loader.js
+++ loader.js
@@ -8,15 +8,15 @@ var special = null;
 var vertClues = null;
 var horClues = null;
 var stripComments = true;
-var indexhtml = '';
+var indexhtml = "";
 
 var showLetters = false;
-var crosswordId = '';
+var crosswordId = "";
 
 var $ = swedish;
 
 var errElm = document.getElementById("errors");
-var qs = location.search.match(/^\??(.*)/)[1], qparts = qs.split('&'), q = {};
+var qs = location.search.match(/^\??(.*)/)[1], qparts = qs.split("&"), q = {};
 for (var part of qparts) {
 	var ind = part.indexOf("=");
 	if (ind !== -1)
@@ -24,33 +24,37 @@ for (var part of qparts) {
 }
 document.title = $.title;
 if (!qs.length) {
-	// errElm.textContent = $.noIdSpecified;
 	document.body.classList.add("indexpage");
 	var cont = document.getElementById("indexpage");
 	var scr = document.createElement("script");
-	scr.src = 'data/index.js?' + Math.random();
+	scr.src = "data/index.js?" + Math.random();
 	scr.onload = function() {
 		cont.innerHTML = indexhtml;
 	};
 	document.body.appendChild(scr);
 } else {
 	crosswordId = qparts[0];
-	document.title += ' #' + crosswordId;
-	var scr = document.createElement("script");
-	if (q['show'] === '1') showLetters = true;
+	document.title += " #" + crosswordId;
+	if (q["show"] === "1") showLetters = true;
 	var cacheBreak = document.currentScript.src.split("?")[1];
 	cacheBreak = cacheBreak ? "?" + cacheBreak : "";
-	var keyStr = q['key'] ? '-' + q['key'] : '';
-	if (!crosswordId.includes('/') && !keyStr.includes('/'))
-		scr.src = 'data/' + crosswordId + keyStr + '.js?2';
-	scr.onload = function() {
+	function loadCrossword(keyStr) {
 		var scr = document.createElement("script");
-		scr.src = 'crossword.js?' + cacheBreak;
+		if (!crosswordId.includes("/"))
+			scr.src = "data/" + crosswordId + keyStr + ".js?2";
+		scr.onload = function() {
+			var scr = document.createElement("script");
+			scr.src = "crossword.js?" + cacheBreak;
+			document.body.appendChild(scr);
+			document.title = $.title + " #" + crosswordId;
+		};
+		scr.onerror = function() {
+			if (keyStr)
+				loadCrossword("");
+			else
+				errElm.textContent = $.noSuchCrossword;
+		};
 		document.body.appendChild(scr);
-		document.title = $.title + ' #' + crosswordId;
-	};
-	scr.onerror = function() {
-		errElm.textContent = $.noSuchCrossword;
-	};
-	document.body.appendChild(scr);
+	}
+	loadCrossword(q["key"] && !q["key"].includes("/") ? "-" + q["key"] : "");
 }
